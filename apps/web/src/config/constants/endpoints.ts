7d5093bc7555c50c857181248febf742943062ea
Shahzeb Khan <shahzeb8285@gmail.com>
2023-05-27 05:10:12 +0530

    all good

--- apps/web/src/config/constants/endpoints.ts
+++ apps/web/src/config/constants/endpoints.ts
@@ -47,6 +47,6 @@ export const BLOCKS_CLIENT_WITH_CHAIN = {
   [ChainId.ETHEREUM]: BLOCKS_CLIENT_ETH,
 }
 
-export const ASSET_CDN = 'https://assets.pancakeswap.finance'
+export const ASSET_CDN = ''
 
 export const TRADING_REWARD_API = 'https://pancake-trading-fee-rebate-api.pancake.run/api/v1'
