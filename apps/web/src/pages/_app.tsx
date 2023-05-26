22ff5d1fc03c707cf2ca5b6abbef7e2a4ee22d2b
Shahzeb Khan <shahzeb8285@gmail.com>
2023-05-27 05:11:09 +0530

    all good

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -75,9 +75,9 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
+          content="CREATE, TRADE & EARN CanaryXswap Your gateway to the decentralized finance movement on the Songbird Network."
         />
-        <meta name="theme-color" content="#1FC7D4" />
+        {/* <meta name="theme-color" content="#1FC7D4" /> */}
         {(Component as NextPageWithLayout).mp && (
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script src="https://public.bnbstatic.com/static/js/mp-webview-sdk/webview-v1.0.0.min.js" id="mp-webview" />
