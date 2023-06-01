1eb63dc2f8082d9e0f9e867b7dc85fa176471ae9
Shahzeb Khan <shahzeb8285@gmail.com>
2023-06-01 20:36:47 +0530

    remove liquidity fixed

--- apps/web/src/views/RemoveLiquidity/index.tsx
+++ apps/web/src/views/RemoveLiquidity/index.tsx
@@ -180,7 +180,7 @@ export default function RemoveLiquidity({ currencyA, currencyB, currencyIdA, cur
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'CANARY LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
